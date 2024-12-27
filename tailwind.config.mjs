1323866f901ff18b8390d686e61d7cb750069f4d
yolbert28 <yolberttorrealba27@gmail.com>
2024-12-27 14:53:55 -0400

    my first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+		'primary-gray': '#0E0E11',
+		'secondary-gray': '#373843',
+		'tertiary-gray': '#595A7033',
+		'text-gray': '#D7D8E0',
+		'primary-blue': '#1064FF',
+		'secondary-blue': '#D7EAFF',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
